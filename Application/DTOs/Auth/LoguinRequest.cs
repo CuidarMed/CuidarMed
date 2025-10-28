5346ba4db437b26f68122b7a9483797db35cc48c
Cristian <cristianseg0122@gmail.com>
2025-10-28 14:19:37 -0300

    Agrega endpoints de funcionalidad para patient

---
+++ Application/DTOs/Auth/LoguinRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Auth
+{
+    public class LoginRequest
+    {
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Password { get; set; }
+    }
+}
+ 
